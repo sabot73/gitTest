c4f962893d2f8162b349f72cf79263cb828af5a5
hyungsoo.lim <hyungsoo.lim@atoncorp.com>
2021-08-14 10:01:34 +0900

    make test2.java remake

---
+++ src/test2.java
@@ -0,0 +1,8 @@
+public class test2 {
+    public static void  main(String[] args){
+
+        System.out.println("*******************");
+        System.out.println("*******test 8 14 day *******");
+        System.out.println("*******************");
+    }
+}
